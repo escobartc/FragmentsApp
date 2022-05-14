aaec43a9ee3d15f2af2efdd9f2e4edc92ecece96
Carlos Escobar Triana <55846521+escobartc@users.noreply.github.com>
2022-05-14 17:58:05 -0500

    movie view model created

--- app/src/main/java/com/example/fragmentsapp/MovieViewModel.kt
+++ app/src/main/java/com/example/fragmentsapp/MovieViewModel.kt
@@ -1,9 +1,24 @@
 package com.example.fragmentsapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.fragmentsapp.model.Movie
 
 class MovieViewModel():ViewModel(){
 
 
+    private val users: MutableLiveData<List<Movie>> by lazy {
+        MutableLiveData<List<Movie>>().also {
+            loadMovies()
+        }
+    }
 
+    fun getUsers(): LiveData<List<Movie>> {
+        return users
+    }
+
+    private fun loadMovies() {
+        // Do an asynchronous operation to fetch users.
+    }
 }
